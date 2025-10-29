5a829c430fde3ee5217e894b993a95919c7b7861
Jason <95380767+Jaiwin42@users.noreply.github.com>
2025-10-29 12:50:11 -0500

    Done

--- Intermediate_Program_ComplexPrimitive/FindRoots.go
+++ Intermediate_Program_ComplexPrimitive/FindRoots.go
@@ -2,27 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
 	"os"
 	"strconv"
 )
 
-func main() {
+func getUserInput() [3]float64 {
 
 	//collecting user input
 	var inputBuf string = ""
-	fmt.Println("How many terms does your function have?(up to 3): ")
-	fmt.Scanln(&inputBuf)
+	fmt.Println("Please Input you function terms, Starting with x^2 term, in the form y = (3)x^2 + (2)x + (1): ")
 
-	numTerms, err := strconv.Atoi(inputBuf)
-	if err != nil {
-		os.Exit(1)
-	}
+	var function [3]float64
 
-	var i int = numTerms
-	function := make([]float64, numTerms)
-
+	var i int = 2
 	for i >= 0 {
-		fmt.Println("Input term " + strconv.Itoa(i) + " of your function(highest order first): ")
+		fmt.Println("Input term " + strconv.Itoa(i+1) + " of your function: ")
 		fmt.Scanln(&inputBuf)
 
 		value, err := strconv.ParseFloat(inputBuf, 64)
@@ -31,9 +26,33 @@ func main() {
 		}
 
 		function[i] = value
-		i++
+		i--
 	}
 
-	//starting function processing.
+	return function
 
+}
+
+func getRoots(function [3]float64) {
+	//does the quadratic formula with the variable names from the equation
+	var a float64 = function[2]
+	var b float64 = function[1]
+	var c float64 = function[0]
+
+	var root1 complex128
+	var root2 complex128
+
+	var determinant complex128 = cmplx.Sqrt(complex((b*b)-(4*a*c), 0))
+
+	root1 = (-complex(b, 0) + determinant) / (2 * complex(a, 0))
+	root2 = (-complex(b, 0) - determinant) / (2 * complex(a, 0))
+
+	fmt.Println("First Root: ", root1)
+	fmt.Println("Second Root: ", root2)
+
+}
+
+func main() {
+	var function [3]float64 = getUserInput()
+	getRoots(function)
 }

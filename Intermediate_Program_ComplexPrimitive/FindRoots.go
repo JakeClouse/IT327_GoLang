26404bbea51a1d002476abe3ea7a25a4ca0e345c
Jason <95380767+Jaiwin42@users.noreply.github.com>
2025-10-29 00:38:04 -0500

    some stuff not done

--- Intermediate_Program_ComplexPrimitive/FindRoots.go
+++ Intermediate_Program_ComplexPrimitive/FindRoots.go
@@ -1,7 +1,38 @@
 package main
 
 import "fmt"
+import "strconv"
 
 func main() {
-	fmt.Println("Hello, world")
+
+	//collecting user input
+	var input String = ""
+	fmt.Println("How many terms does your function have?(up to 3): ")
+	fmt.ScanLn(&input)
+
+	var numTerms int = strconv.Atoi(input)
+
+	var i int = numTerms
+	var function [numTerms]double = []
+
+	while (i >= 0){
+		fmt.Println("Input term " + i + " of your function(highest order first): ")
+		fmt.ScanLn(&input[i])
+	}
+
+
+	//starting function processing.
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

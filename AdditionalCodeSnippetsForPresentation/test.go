6e88b92c384df3e38248ee556a55610dec5bfa13
Jason <95380767+Jaiwin42@users.noreply.github.com>
2025-11-15 15:08:43 -0600

    Some Sample Code for powerpoint

---
+++ AdditionalCodeSnippetsForPresentation/test.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		var x int = 2
+		var y float64 = 2.5
+		var z float64 = y + x
+		fmt.Println(z)
+	*/
+
+	/*
+		x := 2
+		y := 2.5
+		z := x+y
+		fmt.Println(z)
+	*/
+
+	var x rune = 'ඞ'
+	fmt.Printf("%c", x)
+
+}

32ba717ad6ac841092ed1afc404cfbe15e5af0ba
Jason <95380767+Jaiwin42@users.noreply.github.com>
2025-11-18 15:15:50 -0600

    Testing

--- AdditionalCodeSnippetsForPresentation/test.go
+++ AdditionalCodeSnippetsForPresentation/test.go
@@ -2,22 +2,38 @@ package main
 
 import "fmt"
 
+func incrementAndAdd(a int) func(b int) int {
+	return func(b int) int {
+		a++
+		return a + b
+	}
+}
+
 func main() {
-	/*
-		var x int = 2
-		var y float64 = 2.5
-		var z float64 = y + x
-		fmt.Println(z)
-	*/
 
-	/*
-		x := 2
-		y := 2.5
-		z := x+y
-		fmt.Println(z)
-	*/
+	fmt.Println("Function Currying: ")
 
+	newFunction := incrementAndAdd(5)
+	fmt.Println(newFunction(10))
+
+	newFunction2 := incrementAndAdd(10)
+	fmt.Println(newFunction2(20))
+}
+
+/*
+	var x int = 2
+	var y float64 = 2.5
+	var z float64 = y + x
+	fmt.Println(z)
+*/
+
+/*
+	x := 2
+	y := 2.5
+	z := x+y
+	fmt.Println(z)
+*/
+/*
 	var x rune = 'ඞ'
 	fmt.Printf("%c", x)
-
-}
+*/
